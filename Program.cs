9b15bcb49f5ebf47cdd136da6136182fefeff4a9
chinmay <chinmaymhatre12@gmail.com>
2021-01-11 20:28:16 +0530

    created  my app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MLH!");
+            var name = "Chinmay";
+            Console.WriteLine("My name is "+name);
+        }
+    }
+}
